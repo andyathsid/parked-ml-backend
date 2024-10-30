03fc30116793ee4e9837c6da8d3f669b9d9217c8
andyathsid <andakaraas@gmail.com>
2024-10-30 11:40:36 +0700

    Refactor test script as well for the new lambda function script

--- predict-test.py
+++ predict-test.py
@@ -4,16 +4,15 @@ import base64
 
 url = 'http://localhost:8080/2015-03-31/functions/function/invocations'
 
-file_path = './data/raw/FB1cdaopmoe67M2605161917.wav'
+# file_path = './data/raw/FB1cdaopmoe67M2605161917.wav'
 
-with open(file_path, 'rb') as file:
-    file_content = file.read()
+# with open(file_path, 'rb') as file:
+#     file_content = file.read()
 
-encoded_file = base64.b64encode(file_content).decode('utf-8')
+# encoded_file = base64.b64encode(file_content).decode('utf-8')
 
 payload = {
-    'file': encoded_file,
-    'filename': 'FB1cdaopmoe67M2605161917.wav'  
+    'vm-url': 'https://raw.githubusercontent.com/andyathsid/parked-ml-backend/refs/heads/dev/data/raw/FB1cdaopmoe67M2605161917.wav', 
 }
 
 headers = {
